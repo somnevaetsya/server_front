2445ca5a3ee6a59fbccf34b2e1d0c28479f205ce
sasha <yalublukotikov@yandex.ru>
2022-02-28 19:44:56 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+
+const SERVER_PORT = 3000
+
+const server = http.createServer((req, res)=>{
+    const {url} = req;
+    console.log('request', url);
+
+    const fileName = url === '/' ? 'index.html' : url;
+
+    fs.readFile(`${__dirname}/../${fileName}`, (err, file) =>{
+        if (err){
+            console.log('error');
+            res.write('404 not found');
+            res.end();
+            return;
+        }
+
+        res.write(file)
+        res.end();
+    });
+});
+
+server.listen(SERVER_PORT)
