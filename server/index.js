982040c4d4ecd46ade8ce800fe68bfe3b6fbce96
sasha <yalublukotikov@yandex.ru>
2022-03-01 12:16:53 +0300

    awd

--- server/index.js
+++ server/index.js
@@ -1,18 +1,28 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
 const SERVER_PORT = 3000
 
-const server = http.createServer((req, res)=>{
+
+const server = http.createServer((req, res) => {
     const {url} = req;
     console.log('request', url);
 
-    const fileName = url === '/' ? 'src/template/index.html' : url;
+    const fileName = url === '/' ? 'index.html' : url;
 
-    fs.readFile(`${__dirname}/../${fileName}`, (err, file) =>{
-        if (err){
+    const extension = fileName.split('.').pop()
+    let fileDir = 'template'
+    if (extension == 'js') {
+        fileDir = 'js'
+    } else if (extension == 'css') {
+        fileDir = 'css'
+    }
+    fs.readFile(`${__dirname}/../src/${fileDir}/${fileName}`, (err, file) => {
+        if (err) {
             console.log('error');
             res.write('404 not found');
+            res.write(`${__dirname}`);
             res.end();
             return;
         }
