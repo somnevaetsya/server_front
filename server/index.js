21be9e260a2f128a469706f423ee767fb393631a
sasha <yalublukotikov@yandex.ru>
2022-03-01 11:54:07 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -4,22 +4,22 @@ const path = require('path')
 
 const SERVER_PORT = 3000
 
-const server = http.createServer((req, res)=>{
+
+const server = http.createServer((req, res) => {
     const {url} = req;
     console.log('request', url);
 
     const fileName = url === '/' ? 'index.html' : url;
 
     const extension = fileName.split('.').pop()
-
     let fileDir = 'template'
-    if (extension == 'js'){
+    if (extension == 'js') {
         fileDir = 'js'
-    } else if (extension == 'css'){
+    } else if (extension == 'css') {
         fileDir = 'css'
     }
-    fs.readFile(`${__dirname}/../src/${fileDir}/${fileName}`, (err, file) =>{
-        if (err){
+    fs.readFile(`${__dirname}/../src/${fileDir}/${fileName}`, (err, file) => {
+        if (err) {
             console.log('error');
             res.write('404 not found');
             res.write(`${__dirname}`);
